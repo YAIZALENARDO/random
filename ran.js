e09ce2665509aeb94ad98ef13a0370151a1caf23
YAIZALENARDO <111409323+YAIZALENARDO@users.noreply.github.com>
2023-02-10 17:10:59 +0700

    Update ran.js

--- ran.js
+++ ran.js
@@ -1,4 +1,4 @@
-var threads = ['2023/02/gadis-hijaber-cantik-jogja.html', '2023/02/gadis-belia-berhijab-cantik-banget.html', '2023/02/hijaber-gadis-idaman-ibu.html', '2023/02/cewek-hijaber-gaul-cantik.html','2023/02/ig-cewek-cantik-berjilbab.html','2023/02/generasi-tiktok-cantik.html',,];  
+var threads = ['2023/02/gadis-hijaber-cantik-jogja.html', '2023/02/gadis-belia-berhijab-cantik-banget.html', '2023/02/hijaber-gadis-idaman-ibu.html', '2023/02/cewek-hijaber-gaul-cantik.html','2023/02/ig-cewek-cantik-berjilbab.html','2023/02/generasi-tiktok-cantik.html','',];  
 var random  = threads[Math.floor(Math.random() * threads.length)];
 
 setTimeout(function(){
