2fff67abd1df64f0ffc53adcfada5db087cf480c
YAIZALENARDO <111409323+YAIZALENARDO@users.noreply.github.com>
2023-02-10 17:06:55 +0700

    Update ran.js

--- ran.js
+++ ran.js
@@ -1,6 +1,6 @@
-var threads = ['4128749', '5128749', '3128749', ...];  
+var threads = ['2023/02/gadis-hijaber-cantik-jogja.html', '2023/02/gadis-belia-berhijab-cantik-banget.html', '2023/02/hijaber-gadis-idaman-ibu.html', '2023/02/cewek-hijaber-gaul-cantik.html','2023/02/ig-cewek-cantik-berjilbab.html','2023/02/generasi-tiktok-cantik.html',,];  
 var random  = threads[Math.floor(Math.random() * threads.length)];
 
 setTimeout(function(){
-    window.location.href = 'http://somesite.xxx/showthread.php?tid=' + random;
+    window.location.href = 'https://api-onyourway.blogspot.com/' + random;
 }, 10000);
